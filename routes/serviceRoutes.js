f1a49dc147e55235cc2a64d8d12a603c02bd47e5
Alejandro Guinot <al375927@uji.es>
2023-02-14 15:28:59 +0100

    Refactor del rutado de servicios (solo GETs)

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -4,44 +4,6 @@ const router = Router();
 const { db } = require('../firebase')
 
 
-// Añadir un servicio a una ubicacion
-router.post('/:tipo/:ubiid/:usu', async (req, res) => {
-    const tipo = req.params.tipo
-    const id = req.params.ubiid
-    const user = req.params.usu
-    const colRef = await db.collection('col2-servicios')
-    const query = await colRef
-        .where('usuario', '==', user)
-        .where('id_ubicacion', '==', id)
-        .where('tipo_servicio', '==', tipo)
-    const call = await query.get()
-    if (!call.empty) {
-        res.send('Ya existe el servicio')
-    } else {
-        await colRef.doc().set({
-            usuario: user,
-            id_ubicacion: id,
-            tipo_servicio: tipo,
-            disabled: false
-        })
-        res.send('El servicio se ha añadido a la base de datos')
-    }
-})
-
-// Eliminar un servicio a una ubicacion
-router.delete('/:servid', async (req, res) => {
-    const id = req.params.servid
-    const colRef = await db.collection('col2-servicios')
-    const docRef = await colRef.doc(id)
-    const call = await docRef.get()
-    if (!call.exists) {
-        res.send(`El servicio ${id} no existe`)
-    } else {
-        await docRef.delete()
-        res.send(`${id} borrado`)
-    }
-})
-
 // Consultar qué servicios globales hay en la app
 router.get('/globals', async (req, res) => {
     const colRef = await db.collection('col3-serviciosGlobales')
@@ -52,11 +14,11 @@ router.get('/globals', async (req, res) => {
         const data = doc.data()
         services.push({ id, data })
     })
-    res.send({ services })
+    res.status(200).json({ services })
 })
 
 // Consultar qué servicios tiene una ubicacion de un usuario
-router.get('/:usu/:ubiid', async (req, res) => {
+router.get('/consultaServicios/:usu/:ubiid', async (req, res) => {
     const user = req.params.usu
     const id = req.params.ubiid
     const colRef = await db.collection('col2-servicios')
@@ -65,7 +27,7 @@ router.get('/:usu/:ubiid', async (req, res) => {
         .where('id_ubicacion', '==', id)
     const call = await query.get()
     if (call.empty) {
-        res.send(`La ubicacion ${id} no tiene servicios`)
+        res.status(400).send(`La ubicacion ${id} no tiene servicios`)
     } else {
         let services = []
         call.forEach((doc) => {
@@ -73,109 +35,9 @@ router.get('/:usu/:ubiid', async (req, res) => {
             const data = doc.data()
             services.push({ id, data })
         })
-        res.send({ services })
+        res.status(200).json({ services })
     }
 })
-
-// Activar o desactivar el servicio de una ubicacion
-router.put('/:servid', async (req, res) => {
-    const id = req.params.servid
-    const colRef = await db.collection('col2-servicios')
-    const docRef = await colRef.doc(id)
-    const call = await docRef.get()
-    if (!call.exists) {
-        res.send('No existe el servicio')
-    } else {
-        const current = call.data().disabled
-        const newValue = !current
-        await docRef.update({
-            disabled: newValue
-        })
-        res.send(`Servicio ${id} actualizado con éxito`)
-    }
-})
-
-// Activar los servicios de un tipo que tiene el usuario (API selector)
-// TODO
-// Arreglar o refactor
-router.put('/apiselectorActivar/:usu/:tipo', async (req, res) => {
-    const user = req.params.usu
-    const tipo = req.params.tipo
-    const docRef = await db
-        .collection('col4-serviciosLocalesUsuarios')
-        .doc(user)
-    const call = await docRef.get()
-    const current = call.get(`${tipo}`)
-    const newValue = !current
-    if (tipo === 'eventos') {
-        await docRef.update({
-            eventos: newValue
-        })
-    } else if (tipo === 'noticias') {
-        await docRef.update({
-            noticias: newValue
-        })
-    } else if (tipo === 'tiempo') {
-        await docRef.update({
-            tiempo: newValue
-        })
-    }
-    await actualizarServiciosUsuario(user, tipo)
-    res.send('Cambios selector API hechos')
-})
-
-// Desactivar los servicios de un tipo que tiene el usuario (API selector)
-// TODO
-// Arreglar
-router.put('/apiselectorDesactivar/:usu/:tipo', async (req, res) => {
-    const user = req.params.usu
-    const tipo = req.params.tipo
-    const docRef = await db
-        .collection('col4-serviciosLocalesUsuarios')
-        .doc(user)
-    const call = await docRef.get()
-    const current = call.get(`${tipo}`)
-    const newValue = !current
-    if (tipo === 'eventos') {
-        await docRef.update({
-            eventos: newValue
-        })
-    } else if (tipo === 'noticias') {
-        await docRef.update({
-            noticias: newValue
-        })
-    } else if (tipo === 'tiempo') {
-        await docRef.update({
-            tiempo: newValue
-        })
-    }
-    await actualizarServiciosUsuario(user, tipo)
-    res.send('Cambios selector API hechos')
-})
-
-
-// TODO
-// Refactor
-const actualizarServiciosUsuario = async (usuario, tipo) => {
-    const colRef = await db.collection('col2-servicios')
-    const query = await colRef
-        .where('usuario', '==', usuario)
-        .where('tipo_servicio', '==', tipo)
-    const call = await query.get()
-    let servicios = []
-    call.forEach((doc) => {
-        const id = doc.id
-        const data = doc.data()
-        servicios.push({ id, data })
-    })
-    for (let elem of servicios) {
-        const current = elem.data.disabled
-        const newValue = !current
-        await colRef.doc(elem.id).update({
-            disabled: newValue
-        })
-    }
-}
 
 
 module.exports = {
